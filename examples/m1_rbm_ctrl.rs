aba28865576661f16ea1ef4766ec13a4bb7725d4
Rod Conan <rconan@gmto.org>
2022-02-02 00:22:45 +0000

    ADD MOUNT CTRLR TO M1 RBM CTRL EXAMPLE

--- examples/m1_rbm_ctrl.rs
+++ examples/m1_rbm_ctrl.rs
@@ -5,7 +5,8 @@ use fem::{
     FEM,
 };
 use m1_ctrl as m1;
-use std::{iter::once, ops::Deref};
+use mount_ctrl as mount;
+use std::{ops::Deref, time::Instant};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -14,24 +15,34 @@ async fn main() -> anyhow::Result<()> {
     let sim_sampling_frequency = 1000;
     let sim_duration = 10_usize;
 
+    // MOUNT
+    let mut mnt_ctrl = mount::controller::Controller::new();
+    let mut mnt_driver = mount::drives::Controller::new();
     // M1
     let mut hardpoints = m1::hp_dynamics::Controller::new();
     let mut load_cells = m1::hp_load_cells::Controller::new();
     let mut actuators = m1::actuators::segment1::Controller::new();
-
+    // FEM
     let mut state_space = {
         let mut fem = FEM::from_env()?;
         println!("{}", fem);
         //let ins: Vec<_> = (1..=7).chain(once(14)).collect();
         //let outs: Vec<_> = (2..=8).chain(23..=24).collect();
-        fem.keep_inputs(&[1, 14]).keep_outputs(&[23, 24]);
+        fem.keep_inputs(&[1, 11, 12, 14, 16])
+            .keep_outputs(&[20, 21, 23, 24, 25]);
         println!("{}", fem);
         DiscreteModalSolver::<Exponential>::from_fem(fem)
             .sampling(sim_sampling_frequency as f64)
             .proportional_damping(2. / 100.)
+            .inputs_from(&[&mnt_driver])
             .inputs_from(&[&hardpoints])
             .inputs(vec![ios!(M1ActuatorsSegment1)])
             .outputs(vec![ios!(OSSM1Lcl)])
+            .outputs(ios!(
+                OSSAzEncoderAngle,
+                OSSElEncoderAngle,
+                OSSRotEncoderAngle
+            ))
             .outputs(vec![ios!(OSSHardpointD)])
             .build()?
     };
@@ -40,6 +51,9 @@ async fn main() -> anyhow::Result<()> {
     println!("Y sizes: {:?}", state_space.y_sizes);
 
     const M1_RATE: usize = 10;
+
+    let mut mount_controller = Actor::<Vec<f64>, Vec<f64>, 1, 1>::new().tag("Mount Ctrlr");
+    let mut mount_driver = Actor::<Vec<f64>, Vec<f64>, 1, 1>::new().tag("Mount Driver");
 
     let mut rbm_cmd = Initiator::<Vec<f64>, 1>::build().tag("RBM Cmd");
     let mut bm_cmd = Initiator::<Vec<f64>, M1_RATE>::build().tag("BM Cmd");
@@ -50,14 +64,19 @@ async fn main() -> anyhow::Result<()> {
     let mut fem = Actor::<Vec<f64>, Vec<f64>, 1, 1>::new().tag("FEM");
     let mut sink = Terminator::<Vec<f64>, 1>::build().tag("sink");
 
-    channel![rbm_cmd => m1_hardpoints];
+    channel![mount_controller => mount_driver];
+    channel![mount_driver => fem; 3];
+
     channel![fem => sink];
+    channel![fem => (mount_controller,mount_driver); 3];
+
+    channel![rbm_cmd => m1_hardpoints];
     channel![fem => m1_hp_loadcells => m1s1];
     channel![bm_cmd => m1s1];
     channel![m1_hardpoints => (fem, m1_hp_loadcells); 1];
     channel![m1s1 => fem];
 
-    let n_iterations = sim_sampling_frequency*sim_duration;
+    let n_iterations = sim_sampling_frequency * sim_duration;
     let mut signals =
         Signals::new(vec![42], n_iterations).output_signal(0, 0, Signal::Constant(1e-6));
     spawn!(
@@ -66,10 +85,29 @@ async fn main() -> anyhow::Result<()> {
         (m1_hardpoints, hardpoints,),
         (m1_hp_loadcells, load_cells,),
         (m1s1, actuators, vec![vec![0f64; 335]]),
-        (fem, state_space,)
+        (
+            fem,
+            state_space,
+            vec![
+                vec![0f64; 42],
+                vec![0f64; 4],
+                vec![0f64; 6],
+                vec![0f64; 4],
+                vec![0f64; 84],
+            ]
+        ),
+        (mount_controller, mnt_ctrl, vec![vec![0f64; 3]]),
+        (mount_driver, mnt_driver,)
     );
+
+    println!("Starting the model");
+    let now = Instant::now();
+
     let mut logging = Logging::default();
     run!(sink, logging);
+
+    let elapsed = now.elapsed().as_millis();
+    println!("Model run {}s in {}ms ()", sim_duration, elapsed);
 
     let tau = (sim_sampling_frequency as f64).recip();
     let _: complot::Plot = (
@@ -79,7 +117,11 @@ async fn main() -> anyhow::Result<()> {
                 x.iter().map(|x| x * 1e6).collect::<Vec<f64>>(),
             )
         }),
-        complot::complot!("examples/m1_rbm_ctrl.png", xlabel = "Time [s]", ylabel = ""),
+        complot::complot!(
+            "examples/figures/m1_rbm_ctrl.png",
+            xlabel = "Time [s]",
+            ylabel = ""
+        ),
     )
         .into();
 
