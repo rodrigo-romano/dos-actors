ab9f0bf6f6d7d9ba38ae92b76d697756cbf55a60
Rod Conan <rconan@gmto.org>
2022-05-11 13:55:37 +0000

    ADD OPTIONAL PROGRESS BAR TO TIMER CLIETN

--- src/clients/mod.rs
+++ src/clients/mod.rs
@@ -85,36 +85,59 @@ use crate::{
     io::{Data, Read, Write},
     Update,
 };
+use linya::{Bar, Progress};
 use std::{
     any::type_name,
     fmt::Display,
     marker::PhantomData,
     mem::take,
     ops::{Add, Mul, Sub, SubAssign},
-    sync::Arc,
+    sync::{Arc, Mutex},
 };
+
 mod signals;
 #[doc(inline)]
 pub use signals::{Signal, Signals};
 
+struct ProgressBar {
+    progress: Arc<Mutex<Progress>>,
+    bar: Bar,
+}
+
 /// Simple digital timer
-pub struct Timer(usize);
+pub struct Timer {
+    tick: usize,
+    progress_bar: Option<ProgressBar>,
+}
 impl Timer {
     /// Initializes the timer based on the duration in # of samples
     pub fn new(duration: usize) -> Self {
-        Self(1 + duration)
+        Self {
+            tick: 1 + duration,
+            progress_bar: None,
+        }
+    }
+    pub fn progress(self, progress: Arc<Mutex<Progress>>) -> Self {
+        let bar: Bar = progress.lock().unwrap().bar(self.tick, "Timer:");
+        Self {
+            progress_bar: Some(ProgressBar { progress, bar }),
+            ..self
+        }
     }
 }
 impl Update for Timer {
     fn update(&mut self) {
-        self.0 -= 1;
+        self.tick -= 1;
     }
 }
 pub enum Tick {}
 pub type Void = ();
 impl Write<Void, Tick> for Timer {
     fn write(&mut self) -> Option<Arc<Data<Void, Tick>>> {
-        if self.0 > 0 {
+        if let Some(pb) = self.progress_bar.as_mut() {
+            pb.progress.lock().unwrap().inc_and_draw(&pb.bar, 1)
+        }
+        if self.tick > 0 {
             Some(Arc::new(Data::new(())))
         } else {
             None
