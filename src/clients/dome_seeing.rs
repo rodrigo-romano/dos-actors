3ed73df78e16a4436dcf9d900c3024551e51e3c9
Rod Conan <rconan@gmto.org>
2022-06-11 20:00:11 +0000

    DOME SEEING ON DEMAND LOADING

--- src/clients/dome_seeing.rs
+++ src/clients/dome_seeing.rs
@@ -1,7 +1,7 @@
 use glob::{glob, GlobError, PatternError};
 use serde::{Deserialize, Serialize};
 use std::fs::File;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 #[derive(Debug, thiserror::Error)]
 pub enum DomeSeeingError {
@@ -19,7 +19,7 @@ pub type Result<T> = std::result::Result<T, DomeSeeingError>;
 
 //const CFD_SAMPLING_FREQUENCY: f64 = 5f64; // Hz
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Opd {
     pub mean: f64,
     pub values: Vec<f64>,
@@ -28,7 +28,7 @@ pub struct Opd {
 
 pub struct DomeSeeingData {
     time_stamp: f64,
-    opd: Opd,
+    file: PathBuf,
 }
 pub struct DomeSeeing<const N: usize = 1> {
     data: Vec<DomeSeeingData>,
@@ -36,10 +36,12 @@ pub struct DomeSeeing<const N: usize = 1> {
     current_counter: usize,
     next_counter: usize,
     i: usize,
+    y1: Opd,
+    y2: Opd,
 }
 
 impl<const N: usize> DomeSeeing<N> {
-    pub fn load<P: AsRef<str> + std::fmt::Display>(path: P, take: Option<usize>) -> Result<Self> {
+    pub fn new<P: AsRef<str> + std::fmt::Display>(path: P, take: Option<usize>) -> Result<Self> {
         let mut data: Vec<DomeSeeingData> = Vec::with_capacity(2005);
         for entry in
             glob(&format!("{}/optvol/optvol_optvol_*", path))?.take(take.unwrap_or(usize::MAX))
@@ -53,9 +55,10 @@ impl<const N: usize> DomeSeeing<N> {
                 .and_then(|x| x.split("_").last())
                 .and_then(|x| x.parse::<f64>().ok())
                 .expect("failed to parse dome seeing time stamp");
-            let file = File::open(entry?)?;
-            let opd = bincode::deserialize_from(&file)?;
-            data.push(DomeSeeingData { time_stamp, opd });
+            data.push(DomeSeeingData {
+                time_stamp,
+                file: entry?,
+            });
         }
         data.sort_by(|a, b| a.time_stamp.partial_cmp(&b.time_stamp).unwrap());
         let mut counter = Box::new(
@@ -70,10 +73,16 @@ impl<const N: usize> DomeSeeing<N> {
             current_counter: 0,
             next_counter,
             i: 0,
+            y1: Default::default(),
+            y2: Default::default(),
         })
     }
     pub fn len(&self) -> usize {
         self.data.len()
+    }
+    pub fn get(&self, idx: usize) -> Result<Opd> {
+        let file = File::open(&self.data[idx].file)?;
+        Ok(bincode::deserialize_from(&file)?)
     }
 }
 
@@ -84,10 +93,16 @@ impl<const N: usize> Iterator for DomeSeeing<N> {
         if self.i % N == 0 {
             self.current_counter = self.next_counter;
             self.next_counter = self.counter.next().unwrap();
+            self.y1 = self
+                .get(self.current_counter)
+                .expect("failed to load dome seeing data file");
+            self.y2 = self
+                .get(self.next_counter)
+                .expect("failed to load dome seeing data file");
         };
         let alpha = (self.i - i_cfd * N) as f64 / N as f64;
-        let y1 = &self.data[self.current_counter].opd;
-        let y2 = &self.data[self.next_counter].opd;
+        let y1 = &self.y1;
+        let y2 = &self.y2;
         let opd_i = y1
             .values
             .iter()
@@ -111,21 +126,25 @@ mod tests {
     fn load() {
         let n = 3;
         let dome_seeing: DomeSeeing<1> =
-            DomeSeeing::load("/fsx/CASES/zen30az000_OS7/", Some(n)).unwrap();
+            DomeSeeing::new("/fsx/CASES/zen30az000_OS7/", Some(n)).unwrap();
         assert_eq!(n, dome_seeing.len());
     }
 }
 #[test]
 fn next() {
     let n = 3;
-    const N: usize = 2;
+    const N: usize = 5;
     let mut dome_seeing: DomeSeeing<N> =
-        DomeSeeing::load("/fsx/CASES/zen30az000_OS7/", Some(n)).unwrap();
+        DomeSeeing::new("/fsx/CASES/zen30az000_OS7/", Some(n)).unwrap();
     let mut vals = vec![];
-    for _ in 0..9 {
+    for i in 0..20 {
         let opd = dome_seeing.next().unwrap();
-        vals.push(1e6 * opd[123456]);
-        println!("{}", vals.last().unwrap());
+        vals.push(1e9 * opd[123456]);
+        if i % N == 0 {
+            println!("{:9.3} *", vals.last().unwrap());
+        } else {
+            println!("{:9.3}", vals.last().unwrap());
+        }
     }
-    assert_eq!(vals[0], *vals.last().unwrap());
+    //assert_eq!(vals[0], *vals.last().unwrap());
 }
