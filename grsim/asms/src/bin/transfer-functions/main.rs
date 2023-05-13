fc76b9319ae9676a13b1fbdcaa77e0e26e4417e9
Rod Conan <rconan@gmto.org>
2023-05-13 00:29:39 +0000

    ASM TRANSFER FUNCTION MULTIPLEXED

--- grsim/asms/src/bin/transfer-functions/main.rs
+++ grsim/asms/src/bin/transfer-functions/main.rs
@@ -8,13 +8,7 @@ use gmt_dos_clients_io::gmt_m2::asm::segment::{
     AsmCommand, FaceSheetFigure, VoiceCoilsForces, VoiceCoilsMotion,
 };
 use gmt_dos_clients_m2_ctrl::{Calibration, Segment};
-use gmt_fem::{
-    fem_io::{
-        actors_inputs::{MCM2S1FluidDampingF, MCM2S1VCDeltaF},
-        actors_outputs::{M2Segment1AxialD, MCM2S1VCDeltaD},
-    },
-    FEM,
-};
+use gmt_fem::FEM;
 
 use gmt_dos_clients::interface::{Data, Read, UniqueIdentifier, Update, Write};
 use nalgebra as na;
@@ -51,15 +45,13 @@ impl<U: UniqueIdentifier<DataType = Vec<f64>>> Write<U> for Gain {
 }
 
 pub struct Select {
-    index: usize,
-    data: f64,
+    index: Vec<usize>,
+    data: Vec<f64>,
 }
 impl Select {
-    pub fn new(index: usize) -> Self {
-        Self {
-            index,
-            data: Default::default(),
-        }
+    pub fn new(index: Vec<usize>) -> Self {
+        let data = vec![0f64; index.len()];
+        Self { index, data }
     }
 }
 impl Update for Select {
@@ -71,25 +63,35 @@ enum U {}
 enum Y {}
 impl<T: UniqueIdentifier<DataType = Vec<f64>>> Read<T> for Select {
     fn read(&mut self, data: Data<T>) {
-        self.data = *(data.get(self.index).unwrap());
+        self.index
+            .iter()
+            .zip(self.data.iter_mut())
+            .for_each(|(i, v)| {
+                *v = *(data.get(*i).unwrap());
+            });
     }
 }
 impl<T: UniqueIdentifier<DataType = Vec<f64>>> Write<T> for Select {
     fn write(&mut self) -> Option<Data<T>> {
-        Some(Data::new(vec![self.data]))
+        Some(Data::new(self.data.clone()))
     }
 }
+
+const SID: u8 = 7;
+type VCDeltaF = gmt_fem::fem_io::actors_inputs::MCM2S7VCDeltaF;
+type FluidDampingF = gmt_fem::fem_io::actors_inputs::MCM2S7FluidDampingF;
+type VCDeltaD = gmt_fem::fem_io::actors_outputs::MCM2S7VCDeltaD;
+type AxialD = gmt_fem::fem_io::actors_outputs::M2Segment7AxialD;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    #[allow(unused_imports)]
     env!("FEM_REPO");
 
     let sim_sampling_frequency = 8000;
     let n_actuator = 675;
-    let n_step = 8000;
+    let n_step = 1000 + 8000 * 10;
     let n_mode = 496; //env::var("N_KL_MODE").map_or_else(|_| 66, |x| x.parse::<usize>().unwrap());
 
     let repo = Path::new(env!("CARGO_MANIFEST_DIR"))
@@ -100,7 +102,7 @@ async fn main() -> anyhow::Result<()> {
 
     let mut fem = Option::<FEM>::None;
 
-    let sids = vec![1]; //, 2, 3, 4, 5, 6, 7];
+    let sids = vec![SID]; //, 2, 3, 4, 5, 6, 7];
     let calibration_file_name = format!("asms_zonal_{n_mode}kl_calibration.bin");
     let mut asms_calibration = Calibration::try_from(calibration_file_name.as_str())
         .unwrap_or_else(|_| {
@@ -123,53 +125,53 @@ async fn main() -> anyhow::Result<()> {
         });
     asms_calibration.transpose_modes();
 
-    let fem_as_state_space = DiscreteModalSolver::<ExponentialMatrix>::try_from(
-        "fem_state-space.bin",
-    )
-    .unwrap_or_else(|_| {
-        let dss = DiscreteModalSolver::<ExponentialMatrix>::from_fem(
-            fem.unwrap_or(FEM::from_env().expect("failed to load the FEM from `FEM_REPO`")),
-        )
-        .sampling(sim_sampling_frequency as f64)
-        .proportional_damping(2. / 100.)
-        .ins::<MCM2S1VCDeltaF>()
-        .ins::<MCM2S1FluidDampingF>()
-        .outs::<MCM2S1VCDeltaD>()
-        .outs_with::<M2Segment1AxialD>(asms_calibration.modes_t(Some(sids.clone())).unwrap()[0])
-        .use_static_gain_compensation()
-        .build()
-        .expect("failed to build the FEM state space solver");
-        dss.save("fem_state-space.bin")
-            .expect("failed to save the FEM state space solver");
-        dss
-    });
+    let file_name = "asm7-fem_state-space.bin";
+    let fem_as_state_space = DiscreteModalSolver::<ExponentialMatrix>::try_from(file_name)
+        .unwrap_or_else(|_| {
+            let dss = DiscreteModalSolver::<ExponentialMatrix>::from_fem(
+                fem.unwrap_or(FEM::from_env().expect("failed to load the FEM from `FEM_REPO`")),
+            )
+            .sampling(sim_sampling_frequency as f64)
+            .proportional_damping(2. / 100.)
+            .ins::<VCDeltaF>()
+            .ins::<FluidDampingF>()
+            .outs::<VCDeltaD>()
+            .outs_with::<AxialD>(asms_calibration.modes_t(Some(sids.clone())).unwrap()[0])
+            .use_static_gain_compensation()
+            // .truncate_hankel_singular_values(1e-3)
+            .build()
+            .expect("failed to build the FEM state space solver");
+            dss.save(file_name)
+                .expect("failed to save the FEM state space solver");
+            dss
+        });
     println!("{fem_as_state_space}");
     let mut plant: Actor<_> = (fem_as_state_space, "ASM FEM").into();
 
-    let mode = env::args().nth(1).map_or_else(|| 1, |x| x.parse().unwrap());
-    let mut signals = Signals::new(n_mode, n_step).channel(
-        dbg!(mode) - 1,
-        Signal::white_noise()
-            .expect("fishy!")
-            .std_dev(1e-7)
-            .expect("very fishy!"),
-    );
+    let last_mode = env::args().nth(1).map_or_else(|| 1, |x| x.parse().unwrap());
+    let mut mode: Vec<usize> = (0..=dbg!(last_mode) - 1).collect();
+    mode.dedup();
+    let mut signals = mode.iter().fold(Signals::new(n_mode, n_step), |s, i| {
+        s.channel(
+            *i,
+            Signal::white_noise()
+                .expect("fishy!")
+                .std_dev(1e-7)
+                .expect("very fishy!"),
+        )
+    });
     signals.progress();
     let mut asm_setpoint: Initiator<Signals, 1> = (signals, "White Noise").into();
     let mut select_u: Actor<_> = (
-        Select::new(mode - 1),
-        format!(
-            "Select 
-mode #{mode}"
-        ),
+        Select::new(mode.clone()),
+        "Select 
+mode(s)",
     )
         .into();
     let mut select_y: Actor<_> = (
-        Select::new(mode - 1),
-        format!(
-            "Select 
-mode #{mode}"
-        ),
+        Select::new(mode.clone()),
+        "Select 
+mode(s)",
     )
         .into();
 
@@ -179,30 +181,34 @@ mode #{mode}"
     )
         .into();
 
-    let asm = Segment::<1>::builder(n_actuator, asms_calibration.stiffness(1), &mut modes2forces)
-        .build(&mut plant)?;
+    let asm = Segment::<SID>::builder(
+        n_actuator,
+        asms_calibration.stiffness(SID),
+        &mut modes2forces,
+    )
+    .build(&mut plant)?;
 
     let mut plant_logger: Terminator<_> = Arrow::builder(n_step).build().into();
 
     asm_setpoint
         .add_output()
         .multiplex(2)
-        .build::<AsmCommand<1>>()
+        .build::<AsmCommand<SID>>()
         .into_input(&mut modes2forces)
         .into_input(&mut select_u)?;
     select_u
         .add_output()
         .build::<U>()
-        .logn(&mut plant_logger, 1)
+        .logn(&mut plant_logger, mode.len())
         .await?;
     plant
         .add_output()
-        .build::<FaceSheetFigure<1>>()
+        .build::<FaceSheetFigure<SID>>()
         .into_input(&mut select_y)?;
     select_y
         .add_output()
         .build::<Y>()
-        .logn(&mut plant_logger, 1)
+        .logn(&mut plant_logger, mode.len())
         .await?;
 
     (asm + asm_setpoint + modes2forces + plant + plant_logger + select_u + select_y)
