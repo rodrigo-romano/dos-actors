452bd37f0b1131d8aa6033f4075b69f68b88f2ee
Rod Conan <rconan@gmto.org>
2023-09-12 19:29:18 +0000

    GMT_DOS-CLIENTS LEFTRIGHT CLIENT
    A client to split vector or vector chunks in 2 parts

--- clients/interface/src/lib.rs
+++ clients/interface/src/lib.rs
@@ -4,8 +4,8 @@ mod clients;
 pub use clients::Gain;
 #[cfg(feature = "clients")]
 pub use clients::{
-    Average, Integrator, Logging, OneSignal, Pulse, Sampler, Signal, Signals, Smooth, Source, Tick,
-    Timer, Weight,
+    leftright, Average, Integrator, Logging, Mem, OneSignal, Pulse, Sampler, Signal, Signals,
+    Smooth, Source, Tick, Timer, Weight,
 };
 #[cfg(feature = "interface")]
 pub mod interface;
