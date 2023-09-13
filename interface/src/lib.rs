23111efc7297d7239bf4812714b8d039c9f63dde
Rod Conan <rconan@gmto.org>
2023-09-13 12:23:57 -0700

    GMT_DOS-ACTORS-CLIENTS_INTERFACE
    Move the interface to its own crate

---
+++ interface/src/lib.rs
@@ -0,0 +1,73 @@
+use std::any::type_name;
+
+mod data;
+pub use data::Data;
+pub use dos_uid_derive::UID;
+pub mod units;
+
+pub type Assoc<U> = <U as UniqueIdentifier>::DataType;
+
+/// Defines the data type associated with unique identifier data type
+pub trait UniqueIdentifier: Send + Sync {
+    const PORT: u32 = 50_000;
+    type DataType;
+}
+
+pub trait TimerMarker {}
+
+/// Actor client state update interface
+pub trait Update {
+    fn update(&mut self) {}
+}
+/// Client input data reader interface
+pub trait Read<U: UniqueIdentifier> {
+    /// Read data from an input
+    fn read(&mut self, data: Data<U>);
+}
+/// Client output data writer interface
+pub trait Write<U: UniqueIdentifier> {
+    fn write(&mut self) -> Option<Data<U>>;
+}
+/// Interface for IO data sizes
+pub trait Size<U: UniqueIdentifier> {
+    fn len(&self) -> usize;
+}
+
+pub trait Who<T> {
+    /// Returns type name
+    fn who(&self) -> String {
+        type_name::<T>().to_string()
+    }
+    fn highlight(&self) -> String {
+        let me = <Self as Who<T>>::who(&self);
+        paris::formatter::colorize_string(format!("<italic><on-bright-cyan>{}</>", me))
+    }
+    fn lite(&self) -> String {
+        let me = <Self as Who<T>>::who(&self);
+        paris::formatter::colorize_string(format!("<italic><bright-cyan>{}</>", me))
+    }
+}
+
+use log::{info, warn};
+
+/// Pretty prints error message
+pub fn print_info<S: Into<String>>(msg: S, e: Option<&dyn std::error::Error>) {
+    if let Some(e) = e {
+        let mut msg: Vec<String> = vec![msg.into()];
+        msg.push(format!("{}", e));
+        let mut current = e.source();
+        while let Some(cause) = current {
+            msg.push(format!("{}", cause));
+            current = cause.source();
+        }
+        warn!("{}", msg.join("\n .due to: "))
+    } else {
+        info!("{}", msg.into())
+    }
+}
+
+/// Interface for data logging types
+pub trait Entry<U: UniqueIdentifier> {
+    /// Adds an entry to the logger
+    fn entry(&mut self, size: usize);
+}
---
+++ interface/src/units.rs
@@ -0,0 +1,154 @@
+//! # Units conversion
+//!
+//! Converts data given in the [MKS] system of units
+//!
+//! The conversion is performed by wrapping a type `U` in one of the 4 conversion types: [NM], [MuM], [Arcsec], [Mas]
+//! e.g. `NM<U>` will apply the conversion to nanometers.
+//!
+//! `U` must implements the [UniqueIdentifier] trait with `Vec` as [UniqueIdentifier::DataType]
+//! i.e. the bound on `U` is  `U: UniqueIdentifier<DataType = Vec<T>>`
+//!
+//!
+//! [MKS]: https://en.wikipedia.org/wiki/MKS_system_of_units
+
+use std::{any::type_name, f64::consts::PI, marker::PhantomData, ops::Mul};
+
+use super::{Data, UniqueIdentifier, Update, Write};
+
+/// Trait performing the units conversion
+pub trait UnitsConversion {
+    /// Conversion scale factor
+    const UNITS: f64;
+    type ID: UniqueIdentifier;
+
+    /// Converts data given in MKSA system
+    fn conversion<T>(data: &Data<Self::ID>) -> Result<Data<Self>, String>
+    where
+        Self::ID: UniqueIdentifier<DataType = Vec<T>>,
+        T: Copy + TryFrom<f64> + Mul<T, Output = T>,
+        <T as TryFrom<f64>>::Error: std::fmt::Debug,
+        Self: UniqueIdentifier<DataType = Vec<T>> + Sized,
+    {
+        let msg = format!(
+            "failed to convert f64 to {} in Write<{}>::write",
+            type_name::<T>(),
+            type_name::<Self>()
+        );
+        let s: T = T::try_from(Self::UNITS).map_err(|_| msg)?;
+        let data: Vec<_> = Into::<&[T]>::into(data).iter().map(|x| *x * s).collect();
+        Ok(data.into())
+    }
+}
+
+/// Blanket implementation of [Write] for types that implement [Update] and [Write]
+impl<T, U, C, W> Write<W> for C
+where
+    T: Copy + TryFrom<f64> + Mul<T, Output = T>,
+    <T as TryFrom<f64>>::Error: std::fmt::Debug,
+    U: UniqueIdentifier<DataType = Vec<T>>,
+    C: Update + Write<U>,
+    W: UniqueIdentifier<DataType = Vec<T>> + UnitsConversion<ID = U>,
+{
+    fn write(&mut self) -> Option<Data<W>> {
+        <C as Write<U>>::write(self)
+            .as_ref()
+            .map(|data| <W as UnitsConversion>::conversion(data).unwrap())
+    }
+}
+
+/// Blanket implementation of [UniqueIdentifier] for types that implement [UnitsConversion]
+impl<U, W> UniqueIdentifier for W
+where
+    U: UniqueIdentifier,
+    W: UnitsConversion<ID = U> + Send + Sync,
+{
+    const PORT: u32 = <U as UniqueIdentifier>::PORT;
+    type DataType = <U as UniqueIdentifier>::DataType;
+}
+
+/// Conversion to nanometers
+pub struct NM<U>(PhantomData<U>);
+impl<U: UniqueIdentifier> UnitsConversion for NM<U> {
+    const UNITS: f64 = 1e9;
+    type ID = U;
+}
+
+/// Conversion to micrometers
+pub struct MuM<U: UniqueIdentifier>(PhantomData<U>);
+impl<U: UniqueIdentifier> UnitsConversion for MuM<U> {
+    const UNITS: f64 = 1e6;
+    type ID = U;
+}
+
+/// Conversion to arcseconds
+pub struct Arcsec<U: UniqueIdentifier>(PhantomData<U>);
+impl<U: UniqueIdentifier> UnitsConversion for Arcsec<U> {
+    const UNITS: f64 = (180. * 3600.) / PI;
+    type ID = U;
+}
+
+/// Conversion to milli-arcseconds
+pub struct Mas<U: UniqueIdentifier>(PhantomData<U>);
+impl<U: UniqueIdentifier> UnitsConversion for Mas<U> {
+    const UNITS: f64 = (180. * 3600e3) / PI;
+    type ID = U;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    pub enum W {}
+    impl UniqueIdentifier for W {
+        type DataType = Vec<f64>;
+    }
+    #[derive(Default)]
+    pub struct Client {
+        pub data: Vec<f64>,
+    }
+    impl Update for Client {}
+    impl Write<W> for Client {
+        fn write(&mut self) -> Option<Data<W>> {
+            Some(vec![1e-9, 2e-6, 3e-3].into())
+        }
+    }
+
+    #[derive(Default)]
+    pub struct ClientAngle {
+        pub data: Vec<f64>,
+    }
+    impl Update for ClientAngle {}
+    impl Write<W> for ClientAngle {
+        fn write(&mut self) -> Option<Data<W>> {
+            Some(vec![1., 1e-3].into())
+        }
+    }
+
+    #[test]
+    fn units_nm() {
+        let mut client = Client::default();
+        let data = <Client as Write<NM<W>>>::write(&mut client);
+        dbg!(data);
+    }
+
+    #[test]
+    fn units_mum() {
+        let mut client = Client::default();
+        let data = <Client as Write<MuM<W>>>::write(&mut client);
+        dbg!(data);
+    }
+
+    #[test]
+    fn units_arcsec() {
+        let mut client = ClientAngle::default();
+        let data = <ClientAngle as Write<Arcsec<W>>>::write(&mut client);
+        dbg!(data);
+    }
+
+    #[test]
+    fn units_milli_arcsec() {
+        let mut client = ClientAngle::default();
+        let data = <ClientAngle as Write<Mas<W>>>::write(&mut client);
+        dbg!(data);
+    }
+}
