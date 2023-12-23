2a9fe7f7fa2dd77241a6a5692e9976d70b8ae809
Rod Conan <rconan@gmto.org>
2023-12-22 20:26:11 -0800

    GMT_DOS-CLIENTS_M2-CTRL ASMS-DSL1 TEST
    Fix modal version

--- clients/m2-ctrl/tests/asms-dsl1.rs
+++ clients/m2-ctrl/tests/asms-dsl1.rs
@@ -29,7 +29,7 @@ impl Write<M2ASMAsmCommand> for Multiplex {
     fn write(&mut self) -> Option<Data<M2ASMAsmCommand>> {
         Some(
             self.data
-                .chunks(675)
+                .chunks(6)
                 .map(|data| Arc::new(data.to_vec()))
                 .collect::<Vec<_>>()
                 .into(),
@@ -39,7 +39,7 @@ impl Write<M2ASMAsmCommand> for Multiplex {
 
 /*
 export FEM_REPO=...
-cargo test --release  --package gmt_dos-clients_m2-ctrl --features serde --test asm-dsl -- --exact --nocapture
+cargo test --release  --package gmt_dos-clients_m2-ctrl --features serde --test asms-dsl1 -- --exact --nocapture
 */
 #[tokio::test]
 async fn main() -> anyhow::Result<()> {
@@ -56,7 +56,8 @@ async fn main() -> anyhow::Result<()> {
     let mut kl_modes: Vec<na::DMatrix<f64>> = vec![];
     let mut asms_kl_coefs = vec![];
     let mut asms_nact = vec![];
-    let mut cmd = vec![];
+    // let mut cmd = vec![];
+    let n_mode = 6;
     for i in 1..=7 {
         fem.switch_inputs(Switch::Off, None)
             .switch_outputs(Switch::Off, None);
@@ -73,30 +74,38 @@ async fn main() -> anyhow::Result<()> {
         );
         // println!("{:?}", vc_f2d.shape());
         let mat: na::DMatrix<f64> = MatFile::load(&path)?.var(format!("KL_{i}"))?;
-        let (nact, n_mode) = mat.shape();
+        let (nact, nkl) = mat.shape();
+        assert!(n_mode <= nkl);
         let mut kl_coefs = vec![0.; n_mode];
-        kl_coefs[6] = 1e-8;
-        cmd.push(
-            { &mat * na::DVector::from_column_slice(&kl_coefs) }
-                .as_slice()
-                .to_vec(),
-        );
+        kl_coefs[1] = 1e-8;
+        // cmd.push(
+        //     { &mat * na::DVector::from_column_slice(&kl_coefs) }
+        //         .as_slice()
+        //         .to_vec(),
+        // );
         asms_kl_coefs.push(kl_coefs);
-        asms_nact.push(nact);
-        kl_modes.push(mat);
+        asms_nact.push(n_mode);
+        kl_modes.push(mat.remove_columns(n_mode, nkl - n_mode));
     }
     fem.switch_inputs(Switch::On, None)
         .switch_outputs(Switch::On, None);
     println!("stiffness from FEM in {}ms", now.elapsed().as_millis());
 
+    let kl_modes_t = kl_modes.iter().map(|x| x.transpose()).collect::<Vec<_>>();
+
     let now = Instant::now();
     let plant = DiscreteModalSolver::<ExponentialMatrix>::from_fem(fem)
         .sampling(8e3)
         .proportional_damping(2. / 100.)
-        .including_asms(Some(vec![1, 2, 3, 4, 5, 6, 7]), None, None)?
+        .including_asms(
+            Some(vec![1, 2, 3, 4, 5, 6, 7]),
+            Some(kl_modes.iter().map(|x| x.as_view()).collect()),
+            Some(kl_modes_t.iter().map(|x| x.as_view()).collect()),
+        )?
         .use_static_gain_compensation()
         .build()?;
     println!("plant build up in {}ms", now.elapsed().as_millis());
+    println!("{plant}");
 
     let ks: Vec<_> = vc_f2d.iter().map(|x| Some(x.as_slice().to_vec())).collect();
     let mut asms = SubSystem::new(ASMS::new(asms_nact, ks))
@@ -104,8 +113,8 @@ async fn main() -> anyhow::Result<()> {
         .build()?
         .flowchart();
 
-    let cmd: Vec<_> = cmd.into_iter().flatten().collect();
-    let signal = Signals::from((cmd.as_slice(), 800)); //Signals::new(675 * 7, 800);
+    let cmd: Vec<_> = asms_kl_coefs.clone().into_iter().flatten().collect();
+    let signal = Signals::from((cmd.as_slice(), 8000)); //Signals::new(675 * 7, 800);
     let mx = Multiplex::default();
 
     actorscript!(
@@ -153,9 +162,17 @@ async fn main() -> anyhow::Result<()> {
     ];
 
     for i in 0..7 {
+        /*         let err = (asms_kl_coefs[i]
+        .iter()
+        .zip({ kl_modes[i].transpose() * na::DVector::from_column_slice(&data[0]) }.as_slice())
+        .filter(|(c, _)| c.abs() > 0.)
+        .map(|(&c, &p)| (1. - p / c).powi(2))
+        .sum::<f64>()
+        / 500 as f64)
+        .sqrt(); */
         let err = (asms_kl_coefs[i]
             .iter()
-            .zip({ kl_modes[i].transpose() * na::DVector::from_column_slice(&data[0]) }.as_slice())
+            .zip(data[i].iter())
             .filter(|(c, _)| c.abs() > 0.)
             .map(|(&c, &p)| (1. - p / c).powi(2))
             .sum::<f64>()
