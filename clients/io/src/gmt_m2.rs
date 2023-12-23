0a8e4b5e58b3781e64a4eca269c74ef14097425f
Rod Conan <rconan@gmto.org>
2023-12-22 20:27:36 -0800

    GMT_DOS-CLIENTS_IO FIX ASM SEGMENT PORTS #

--- clients/io/src/gmt_m2.rs
+++ clients/io/src/gmt_m2.rs
@@ -128,31 +128,31 @@ pub mod asm {
         /// Voice coils forces
         pub enum VoiceCoilsForces<const ID: u8> {}
         impl<const ID: u8> UniqueIdentifier for VoiceCoilsForces<ID> {
-            const PORT: u32 = 59_0001 + 100 * ID as u32;
+            const PORT: u32 = 59_001 + 100 * ID as u32;
             type DataType = Vec<f64>;
         }
         /// Voice coils displacements
         pub enum VoiceCoilsMotion<const ID: u8> {}
         impl<const ID: u8> UniqueIdentifier for VoiceCoilsMotion<ID> {
-            const PORT: u32 = 59_0002 + 100 * ID as u32;
+            const PORT: u32 = 59_002 + 100 * ID as u32;
             type DataType = Vec<f64>;
         }
         /// Fluid damping forces
         pub enum FluidDampingForces<const ID: u8> {}
         impl<const ID: u8> UniqueIdentifier for FluidDampingForces<ID> {
-            const PORT: u32 = 59_0003 + 100 * ID as u32;
+            const PORT: u32 = 59_003 + 100 * ID as u32;
             type DataType = Vec<f64>;
         }
         /// Modal command coefficients
         pub enum AsmCommand<const ID: u8> {}
         impl<const ID: u8> UniqueIdentifier for AsmCommand<ID> {
-            const PORT: u32 = 59_0004 + 100 * ID as u32;
+            const PORT: u32 = 59_004 + 100 * ID as u32;
             type DataType = Vec<f64>;
         }
         /// Face sheet displacements
         pub enum FaceSheetFigure<const ID: u8> {}
         impl<const ID: u8> UniqueIdentifier for FaceSheetFigure<ID> {
-            const PORT: u32 = 59_0005 + 100 * ID as u32;
+            const PORT: u32 = 59_005 + 100 * ID as u32;
             type DataType = Vec<f64>;
         }
     }
