639abb98087e3853cb5d16a9e4d2b6b2be6102a5
Rod Conan <rconan@gmto.org>
2024-01-05 14:42:01 -0800

    GMT_DOS-CLIENTS_LOM OPTICALSENSITIVITY CLIENT

--- clients/lom/src/lib.rs
+++ clients/lom/src/lib.rs
@@ -4,7 +4,7 @@
 //! (tip-tilt, segment piston and  segment tip-tilt) using
 //! linear optical sensitivity matrices
 
-use std::{io::Read, sync::Arc};
+use std::{io::Read, path::Path, sync::Arc};
 
 use flate2::bufread::GzDecoder;
 use gmt_dos_clients_io::{
@@ -12,17 +12,20 @@ use gmt_dos_clients_io::{
     gmt_m2::M2RigidBodyMotions,
     optics::{MaskedWavefront, SegmentPiston, SegmentTipTilt, TipTilt, Wavefront, WfeRms},
 };
-use gmt_lom::{LinearOpticalModelError, LOM};
+use gmt_lom::{LinearOpticalModelError, Loader, LOM};
 use interface::{self, Data, Size, Units, Update, Write};
+
+mod optical_sensitivity;
+pub use optical_sensitivity::OpticalSensitivities;
 
 /// M1 & M2 Rigid Body Motions to Linear Optical Model
 #[derive(Debug)]
-pub struct RigidBodyMotionsToLinearOpticalModel {
+pub struct LinearOpticalModel {
     lom: LOM,
     m1_rbm: Arc<Vec<f64>>,
     m2_rbm: Arc<Vec<f64>>,
 }
-impl RigidBodyMotionsToLinearOpticalModel {
+impl LinearOpticalModel {
     pub fn new() -> std::result::Result<Self, LinearOpticalModelError> {
         let sens = include_bytes!("optical_sensitivities.rs.bin.gz");
         let mut gz = GzDecoder::new(sens.as_slice());
@@ -34,56 +37,73 @@ impl RigidBodyMotionsToLinearOpticalModel {
             m2_rbm: Arc::new(vec![0f64; 42]),
         })
     }
+    pub fn new_with_path(
+        path: impl AsRef<Path>,
+    ) -> std::result::Result<Self, LinearOpticalModelError> {
+        let path = path.as_ref();
+        let filename = path.file_name().unwrap();
+        let sens_loader = Loader::<gmt_lom::OpticalSensitivities>::default()
+            .path(path.parent().unwrap())
+            .filename(filename.to_str().unwrap());
+        Ok(Self {
+            lom: LOM::builder()
+                .load_optical_sensitivities(sens_loader)?
+                .build()?,
+            m1_rbm: Arc::new(vec![0f64; 42]),
+            m2_rbm: Arc::new(vec![0f64; 42]),
+        })
+    }
 }
 
-impl Units for RigidBodyMotionsToLinearOpticalModel {}
+impl Units for LinearOpticalModel {}
 
-impl Update for RigidBodyMotionsToLinearOpticalModel {
+impl Update for LinearOpticalModel {
     fn update(&mut self) {
         self.lom.rbm = vec![(self.m1_rbm.as_slice(), self.m2_rbm.as_slice())]
             .into_iter()
             .collect();
     }
 }
-impl interface::Read<M1RigidBodyMotions> for RigidBodyMotionsToLinearOpticalModel {
+impl interface::Read<M1RigidBodyMotions> for LinearOpticalModel {
     fn read(&mut self, data: Data<M1RigidBodyMotions>) {
-        self.m1_rbm = data.as_arc();
+        self.m1_rbm = data.into_arc();
     }
 }
-impl interface::Read<M2RigidBodyMotions> for RigidBodyMotionsToLinearOpticalModel {
+impl interface::Read<M2RigidBodyMotions> for LinearOpticalModel {
     fn read(&mut self, data: Data<M2RigidBodyMotions>) {
-        self.m2_rbm = data.as_arc();
+        self.m2_rbm = data.into_arc();
     }
 }
-impl Write<TipTilt> for RigidBodyMotionsToLinearOpticalModel {
+
+impl Write<TipTilt> for LinearOpticalModel {
     fn write(&mut self) -> Option<Data<TipTilt>> {
         Some(Data::new(self.lom.tiptilt().into()))
     }
 }
-impl Write<SegmentTipTilt> for RigidBodyMotionsToLinearOpticalModel {
+impl Write<SegmentTipTilt> for LinearOpticalModel {
     fn write(&mut self) -> Option<Data<SegmentTipTilt>> {
         Some(Data::new(self.lom.segment_tiptilt().into()))
     }
 }
-impl Write<SegmentPiston> for RigidBodyMotionsToLinearOpticalModel {
+impl Write<SegmentPiston> for LinearOpticalModel {
     fn write(&mut self) -> Option<Data<SegmentPiston>> {
         Some(Data::new(self.lom.segment_piston().into()))
     }
 }
 
-impl Write<Wavefront> for RigidBodyMotionsToLinearOpticalModel {
+impl Write<Wavefront> for LinearOpticalModel {
     fn write(&mut self) -> Option<Data<Wavefront>> {
         Some(Data::new(self.lom.wavefront().into()))
     }
 }
 
-impl Write<MaskedWavefront> for RigidBodyMotionsToLinearOpticalModel {
+impl Write<MaskedWavefront> for LinearOpticalModel {
     fn write(&mut self) -> Option<Data<MaskedWavefront>> {
         Some(Data::new(self.lom.masked_wavefront().into()))
     }
 }
 
-impl<const E: i32> Write<WfeRms<E>> for RigidBodyMotionsToLinearOpticalModel {
+impl<const E: i32> Write<WfeRms<E>> for LinearOpticalModel {
     fn write(&mut self) -> Option<Data<WfeRms<E>>> {
         let wavefront = self.lom.masked_wavefront();
         let n = wavefront.len() as f64;
@@ -102,17 +122,17 @@ impl<const E: i32> Write<WfeRms<E>> for RigidBodyMotionsToLinearOpticalModel {
     }
 }
 
-impl Size<TipTilt> for RigidBodyMotionsToLinearOpticalModel {
+impl Size<TipTilt> for LinearOpticalModel {
     fn len(&self) -> usize {
         2
     }
 }
-impl Size<SegmentTipTilt> for RigidBodyMotionsToLinearOpticalModel {
+impl Size<SegmentTipTilt> for LinearOpticalModel {
     fn len(&self) -> usize {
         14
     }
 }
-impl Size<SegmentPiston> for RigidBodyMotionsToLinearOpticalModel {
+impl Size<SegmentPiston> for LinearOpticalModel {
     fn len(&self) -> usize {
         7
     }
@@ -124,7 +144,7 @@ mod tests {
 
     #[test]
     fn m1_segment_rxy() {
-        let mut rbm2lom = RigidBodyMotionsToLinearOpticalModel::new().unwrap();
+        let mut rbm2lom = LinearOpticalModel::new().unwrap();
         let s = 1e-6;
         let m1_rbm: Vec<_> = (0..7)
             .flat_map(|_| {
@@ -133,15 +153,14 @@ mod tests {
                 v
             })
             .collect();
-        <RigidBodyMotionsToLinearOpticalModel as interface::Read<M1RigidBodyMotions>>::read(
+        <LinearOpticalModel as interface::Read<M1RigidBodyMotions>>::read(
             &mut rbm2lom,
             m1_rbm.into(),
         );
         rbm2lom.update();
-        let mut stt: Vec<f64> =
-            <RigidBodyMotionsToLinearOpticalModel as Write<SegmentTipTilt>>::write(&mut rbm2lom)
-                .unwrap()
-                .into();
+        let mut stt: Vec<f64> = <LinearOpticalModel as Write<SegmentTipTilt>>::write(&mut rbm2lom)
+            .unwrap()
+            .into();
         stt.iter_mut().for_each(|x| *x /= s);
         let stt_mag: Vec<_> = stt[..7]
             .iter()
@@ -153,7 +172,7 @@ mod tests {
 
     #[test]
     fn m2_segment_rxy() {
-        let mut rbm2lom = RigidBodyMotionsToLinearOpticalModel::new().unwrap();
+        let mut rbm2lom = LinearOpticalModel::new().unwrap();
         let s = 1e-6;
         let m1_rbm: Vec<_> = (0..7)
             .flat_map(|_| {
@@ -162,15 +181,14 @@ mod tests {
                 v
             })
             .collect();
-        <RigidBodyMotionsToLinearOpticalModel as interface::Read<M2RigidBodyMotions>>::read(
+        <LinearOpticalModel as interface::Read<M2RigidBodyMotions>>::read(
             &mut rbm2lom,
             m1_rbm.into(),
         );
         rbm2lom.update();
-        let mut stt: Vec<f64> =
-            <RigidBodyMotionsToLinearOpticalModel as Write<SegmentTipTilt>>::write(&mut rbm2lom)
-                .unwrap()
-                .into();
+        let mut stt: Vec<f64> = <LinearOpticalModel as Write<SegmentTipTilt>>::write(&mut rbm2lom)
+            .unwrap()
+            .into();
         stt.iter_mut().for_each(|x| *x /= s);
         let stt_mag: Vec<_> = stt[..7]
             .iter()
@@ -182,7 +200,7 @@ mod tests {
 
     #[test]
     fn m1_segment_txy() {
-        let mut rbm2lom = RigidBodyMotionsToLinearOpticalModel::new().unwrap();
+        let mut rbm2lom = LinearOpticalModel::new().unwrap();
         let s = 1e-6;
         let m1_rbm: Vec<_> = (0..7)
             .flat_map(|_| {
@@ -191,15 +209,14 @@ mod tests {
                 v
             })
             .collect();
-        <RigidBodyMotionsToLinearOpticalModel as interface::Read<M1RigidBodyMotions>>::read(
+        <LinearOpticalModel as interface::Read<M1RigidBodyMotions>>::read(
             &mut rbm2lom,
             m1_rbm.into(),
         );
         rbm2lom.update();
-        let mut stt: Vec<f64> =
-            <RigidBodyMotionsToLinearOpticalModel as Write<SegmentTipTilt>>::write(&mut rbm2lom)
-                .unwrap()
-                .into();
+        let mut stt: Vec<f64> = <LinearOpticalModel as Write<SegmentTipTilt>>::write(&mut rbm2lom)
+            .unwrap()
+            .into();
         stt.iter_mut().for_each(|x| *x /= s);
         let stt_mag: Vec<_> = stt[..7]
             .iter()
