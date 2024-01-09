f5b32a6b9b5f99aaeca3058daccf9151561ad3da
Rod Conan <rconan@gmto.org>
2024-01-08 17:58:04 -0800

    GMT_DOS-CLIENT_IO MOVE POSITION METHOD INTO ASSEMBLY TRAIT

--- clients/m1-ctrl/src/fem/assembly/dispatch.rs
+++ clients/m1-ctrl/src/fem/assembly/dispatch.rs
@@ -27,11 +27,6 @@ impl DispatchIn {
     pub fn new() -> Self {
         Default::default()
     }
-    pub fn position<const ID: u8>(&self) -> Option<usize> {
-        <Self as Assembly>::SIDS
-            .into_iter()
-            .position(|sid| sid == ID)
-    }
 }
 impl Assembly for DispatchIn {}
 impl Update for DispatchIn {}
@@ -43,7 +38,7 @@ impl Read<M1RigidBodyMotions> for DispatchIn {
 }
 impl<const ID: u8> Write<RBM<ID>> for DispatchIn {
     fn write(&mut self) -> Option<Data<RBM<ID>>> {
-        self.position::<ID>().and_then(|idx| {
+        <Self as Assembly>::position::<ID>().and_then(|idx| {
             self.m1_rigid_body_motions
                 .get(idx)
                 .map(|data| data.clone().into())
@@ -58,7 +53,7 @@ impl Read<M1ActuatorCommandForces> for DispatchIn {
 }
 impl<const ID: u8> Write<ActuatorCommandForces<ID>> for DispatchIn {
     fn write(&mut self) -> Option<Data<ActuatorCommandForces<ID>>> {
-        self.position::<ID>().and_then(|idx| {
+        <Self as Assembly>::position::<ID>().and_then(|idx| {
             self.m1_actuator_command_forces
                 .get(idx)
                 .map(|data| data.clone().into())
@@ -73,7 +68,7 @@ impl Read<M1HardpointsMotion> for DispatchIn {
 }
 impl<const ID: u8> Write<HardpointsMotion<ID>> for DispatchIn {
     fn write(&mut self) -> Option<Data<HardpointsMotion<ID>>> {
-        self.position::<ID>().and_then(|idx| {
+        <Self as Assembly>::position::<ID>().and_then(|idx| {
             self.m1_hardpoints_motion
                 .get(idx)
                 .map(|data| data.clone().into())
@@ -96,18 +91,13 @@ impl DispatchOut {
             m1_hardpoints_forces: vec![Default::default(); <Self as Assembly>::N],
         }
     }
-    pub fn position<const ID: u8>(&self) -> Option<usize> {
-        <Self as Assembly>::SIDS
-            .into_iter()
-            .position(|sid| sid == ID)
-    }
 }
 impl Assembly for DispatchOut {}
 impl Update for DispatchOut {}
 
 impl<const ID: u8> Read<ActuatorAppliedForces<ID>> for DispatchOut {
     fn read(&mut self, data: Data<ActuatorAppliedForces<ID>>) {
-        if let Some(idx) = self.position::<ID>() {
+        if let Some(idx) = <Self as Assembly>::position::<ID>() {
             let forces = data.into_arc();
             self.m1_actuator_applied_forces[idx] = forces;
         }
@@ -121,7 +111,7 @@ impl Write<M1ActuatorAppliedForces> for DispatchOut {
 
 impl<const ID: u8> Read<HardpointsForces<ID>> for DispatchOut {
     fn read(&mut self, data: Data<HardpointsForces<ID>>) {
-        if let Some(idx) = self.position::<ID>() {
+        if let Some(idx) = <Self as Assembly>::position::<ID>() {
             let forces = data.into_arc();
             self.m1_hardpoints_forces[idx] = forces;
         }
