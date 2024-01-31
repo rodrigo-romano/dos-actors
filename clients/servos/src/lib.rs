987e2314f29eb293e3a298c9dd9994f40dae1c0d
Rod Conan <rconan@gmto.org>
2024-01-31 10:35:00 -0800

    GMT_DOS-CLIENTS_SERVOS BUILD SCRIPT, FEATURES & DOCS
    Add features to match the `gmt_dos-clients_moount` crate features

--- clients/servos/src/lib.rs
+++ clients/servos/src/lib.rs
@@ -1,8 +1,22 @@
+//! # GMT Servo-Mechanisms
+//!
+//! A dos-actors [system] that combines together a few clients:
+//!   * the GMT [FEM]
+//!   * the GMT [mount] control system
+//!   * the GMT [M1] control system
+//!   * the GMT [M2] control system
+//!
+//! [system]: https://docs.rs/gmt_dos-actors/latest/gmt_dos_actors/system
+//! [FEM]: https://docs.rs/gmt_dos-clients_fem/latest/gmt_dos_clients_fem/
+//! [mount]: https://docs.rs/gmt_dos-clients_mount/latest/gmt_dos_clients_mount/
+//! [M1]: https://docs.rs/gmt_dos-clients_m1-ctrl/latest/gmt_dos_clients_m1_ctrl/
+//! [M2]: https://docs.rs/gmt_dos-clients_m2-ctrl/latest/gmt_dos_clients_m2_ctrl/
+//!
 use gmt_dos_actors::system::Sys;
 
 mod servos;
-// pub use servos::GmtServoMechanisms;
 
+/// GMT servo-mechanisms client
 pub enum GmtServoMechanisms<const M1_RATE: usize, const M2_RATE: usize = 1> {}
 impl<const M1_RATE: usize, const M2_RATE: usize> GmtServoMechanisms<M1_RATE, M2_RATE> {
     pub fn new(
@@ -19,8 +33,13 @@ impl<const M1_RATE: usize, const M2_RATE: usize> GmtServoMechanisms<M1_RATE, M2_
     }
 }
 
+/// GMT FEM client
 pub type GmtFem = gmt_dos_clients_fem::DiscreteModalSolver<gmt_dos_clients_fem::ExponentialMatrix>;
+/// GMT M1 client
 pub type GmtM1 = gmt_dos_clients_m1_ctrl::assembly::DispatchIn;
+/// GMT mount client
 pub type GmtMount<'a> = gmt_dos_clients_mount::Mount<'a>;
+/// GMT M2 positioners client
 pub type GmtM2Hex = gmt_dos_clients_m2_ctrl::positioner::AsmsPositioners;
+/// GMT M2 mirror client
 pub type GmtM2 = gmt_dos_clients_m2_ctrl::assembly::DispatchIn;
