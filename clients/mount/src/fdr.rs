d1c0f31e2df939b79325137152e4c44f399b6535
Rod Conan <rconan@gmto.org>
2024-03-04 07:42:28 -0800

    GMT_DOS-CLIENTS_MOUNT IMPL SERDE FOR MOUNT

--- clients/mount/src/fdr.rs
+++ clients/mount/src/fdr.rs
@@ -4,7 +4,7 @@
 A [gmt_dos-actors] client for the GMT mount control system.
 */
 
-use std::marker::PhantomData;
+use serde::{Deserialize, Serialize};
 
 use gmt_mount_ctrl_controller::MountController;
 use gmt_mount_ctrl_driver::MountDriver;
@@ -12,18 +12,17 @@ use gmt_mount_ctrl_driver::MountDriver;
 mod actors_interface;
 
 /// Mount control system
-pub struct Mount<'a> {
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct Mount {
     drive: MountDriver,
     control: MountController,
-    phantom: PhantomData<&'a MountDriver>,
 }
-impl<'a> Mount<'a> {
+impl Mount {
     /// Returns the mount controller
     pub fn new() -> Self {
         Self {
             drive: MountDriver::new(),
             control: MountController::new(),
-            phantom: PhantomData,
         }
     }
 }
