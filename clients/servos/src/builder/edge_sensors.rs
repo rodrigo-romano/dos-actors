ece36d8f1b0aa0b412d5e3b6c941b9bd0708a983
Rod Conan <rconan@gmto.org>
2024-04-09 16:54:12 -0700

    GMT_DOS-CLIENTS_SERVOS EDGE SENSORS TRANSFORMS
    Add `m1_with` and `m2_with` method to `EdgeSensors` builder to pass
    linear transform matrices

--- clients/servos/src/builder/edge_sensors.rs
+++ clients/servos/src/builder/edge_sensors.rs
@@ -1,12 +1,17 @@
 use gmt_dos_clients_fem::fem_io::actors_outputs::{M2EdgeSensors, OSSM1EdgeSensors};
 use gmt_dos_clients_fem::{DiscreteStateSpace, ExponentialMatrix};
+use nalgebra as na;
 
 use super::Include;
 
-#[derive(Debug, Clone)]
-struct M1EdgeSensor;
-#[derive(Debug, Clone)]
-struct M2EdgeSensor;
+#[derive(Debug, Clone, Default)]
+struct M1EdgeSensor {
+    transform: Option<na::DMatrix<f64>>,
+}
+#[derive(Debug, Clone, Default)]
+struct M2EdgeSensor {
+    transform: Option<na::DMatrix<f64>>,
+}
 
 /**
 Edge sensors builder
@@ -69,8 +74,8 @@ impl EdgeSensors {
     /// ```
     pub fn both() -> Self {
         Self {
-            m1: Some(M1EdgeSensor),
-            m2: Some(M2EdgeSensor),
+            m1: Some(M1EdgeSensor::default()),
+            m2: Some(M2EdgeSensor::default()),
         }
     }
     /// Creates a new [EdgeSensors] builder for M1 edge sensors
@@ -88,9 +93,28 @@ impl EdgeSensors {
     /// ```
     pub fn m1() -> Self {
         Self {
-            m1: Some(M1EdgeSensor),
+            m1: Some(M1EdgeSensor::default()),
             m2: None,
         }
+    }
+    /// Applies a linear transformation to the M1 edge sensors
+    /// ```no_run
+    /// # use gmt_dos_clients_servos::{GmtServoMechanisms, EdgeSensors};
+    /// # use gmt_fem::FEM;
+    /// # const ACTUATOR_RATE: usize = 80; // 100Hz
+    /// # let frequency = 8000_f64; // Hz
+    /// # let fem = FEM::from_env()?;
+    /// let gmt_servos =
+    ///     GmtServoMechanisms::<ACTUATOR_RATE, 1>::new(frequency, fem)
+    ///         .edge_sensors(EdgeSensors::m1().m1_with(nalgebra::DMatrix::<f64>::identity(288,288)))
+    ///         .build()?;
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    /// ```
+    pub fn m1_with(mut self, transform: na::DMatrix<f64>) -> Self {
+        self.m1 = Some(M1EdgeSensor {
+            transform: Some(transform),
+        });
+        self
     }
     /// Creates a new [EdgeSensors] builder for M2 edge sensors
     /// ```no_run
@@ -108,8 +132,27 @@ impl EdgeSensors {
     pub fn m2() -> Self {
         Self {
             m1: None,
-            m2: Some(M2EdgeSensor),
+            m2: Some(M2EdgeSensor::default()),
         }
+    }
+    /// Applies a linear transformation to the M2 edge sensors
+    /// ```no_run
+    /// # use gmt_dos_clients_servos::{GmtServoMechanisms, EdgeSensors};
+    /// # use gmt_fem::FEM;
+    /// # const ACTUATOR_RATE: usize = 80; // 100Hz
+    /// # let frequency = 8000_f64; // Hz
+    /// # let fem = FEM::from_env()?;
+    /// let gmt_servos =
+    ///     GmtServoMechanisms::<ACTUATOR_RATE, 1>::new(frequency, fem)
+    ///         .edge_sensors(EdgeSensors::m2().m2_with(nalgebra::DMatrix::<f64>::identity(48,48)))
+    ///         .build()?;
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    /// ```
+    pub fn m2_with(mut self, transform: na::DMatrix<f64>) -> Self {
+        self.m2 = Some(M2EdgeSensor {
+            transform: Some(transform),
+        });
+        self
     }
 }
 
@@ -122,12 +165,20 @@ impl<'a> Include<'a, EdgeSensors> for DiscreteStateSpace<'a, ExponentialMatrix> 
             return Ok(self);
         };
         let mut state_space = self;
-        if edge_sensors.m1.is_some() {
-            state_space = state_space.outs::<OSSM1EdgeSensors>();
-        }
-        if edge_sensors.m2.is_some() {
-            state_space = state_space.outs::<M2EdgeSensors>();
-        }
+        state_space = match edge_sensors.m1.as_ref() {
+            Some(M1EdgeSensor {
+                transform: Some(transform),
+            }) => state_space.outs_with::<OSSM1EdgeSensors>(transform.as_view()),
+            Some(M1EdgeSensor { transform: None }) => state_space.outs::<OSSM1EdgeSensors>(),
+            _ => state_space,
+        };
+        state_space = match edge_sensors.m2.as_ref() {
+            Some(M2EdgeSensor {
+                transform: Some(transform),
+            }) => state_space.outs_with::<M2EdgeSensors>(transform.as_view()),
+            Some(M2EdgeSensor { transform: None }) => state_space.outs::<M2EdgeSensors>(),
+            _ => state_space,
+        };
         Ok(state_space)
     }
 }
