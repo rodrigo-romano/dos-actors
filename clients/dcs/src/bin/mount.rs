a7ac91d12bd61dc9424b4f266615a71bcd6bcb62
Rod Conan <rconan@gmto.org>
2024-08-12 12:09:17 -0700

    DCS/MOUNT ADD RATE TRANSITION BY HAND

--- clients/dcs/src/bin/mount.rs
+++ clients/dcs/src/bin/mount.rs
@@ -1,5 +1,5 @@
 use gmt_dos_actors::actorscript;
-use gmt_dos_clients::Timer;
+use gmt_dos_clients::{Sampler, Timer};
 use gmt_dos_clients_dcs::{
     mount_trajectory::{
         ImMountTrajectory, MountTrajectory, OcsMountTrajectory, RelativeMountTrajectory,
@@ -12,11 +12,14 @@ use gmt_dos_clients_io::{
     mount::{AverageMountEncoders, MountEncoders, MountSetPoint, MountTorques},
 };
 use gmt_dos_clients_mount::Mount;
-use interface::Tick;
+use interface::{Tick, UID};
 use nanomsg::Socket;
 
 const PULL: &str = "tcp://127.0.0.1:5001";
 const PUSH: &str = "tcp://127.0.0.1:5002";
+
+#[derive(UID)]
+pub enum ScopeAverageMountEncoders {}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -36,20 +39,25 @@ async fn main() -> anyhow::Result<()> {
 
     let metronome: Timer = Timer::new(100);
 
+    let sampler = Sampler::default();
+
     actorscript!(
+        #[model(name = mount)]
         #[labels(fem = "60deg EL\n0deg AZ",
             mount = "Mount Controller\n& Driver Models",
             dcs_pull = "From OCS", dcs_push = "To OCS",
-            scope_mountsetpoint = "Scope", scope_averagemountencoders = "Scope",
-            rmt = "Relative\nTrajectory"
+            scope_mountsetpoint = "Scope", scope_scopeaveragemountencoders = "Scope",
+            rmt = "Relative\nTrajectory",
+            sampler = "1:50"
         )]
         #[images(fem = "gmt-pretty4.png")]
 
-        50: metronome[Tick] -> dcs_pull[OcsMountTrajectory]${3} -> rmt[MountSetPoint]~
+        50: metronome[Tick] -> dcs_pull[OcsMountTrajectory]${3} -> rmt[MountSetPoint]..~
         50: rmt[ImMountTrajectory]${3} -> dcs_push
         1: rmt[MountSetPoint] -> mount[MountTorques] -> fem[MountEncoders]! -> mount
         1: fem[AverageMountEncoders]! -> rmt
-        50: fem[AverageMountEncoders]!~
+        1: fem[AverageMountEncoders]! -> sampler
+        50: sampler[ScopeAverageMountEncoders]..~
     );
 
     Ok(())
