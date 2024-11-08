cb9731585e1df1e0d334ac68ff4c5997e12700bc
Rod Conan <rconan@gmto.org>
2024-11-08 20:25:14 +0000

    GMT_DOS-CLIENTS_IO M1 & M2 GLOBAL TIP-TILT
    Set datatype to `Vec` instead of `[f64;2]`

--- clients/io/src/optics.rs
+++ clients/io/src/optics.rs
@@ -21,18 +21,14 @@ pub enum Wavefront {}
 pub enum TipTilt {}
 
 /// M1 global tip-tilt
+#[derive(UID)]
+#[uid(port = 55_101)]
 pub enum M1GlobalTipTilt {}
-impl UniqueIdentifier for M1GlobalTipTilt {
-    type DataType = [f64; 2];
-    const PORT: u16 = 55_101;
-}
 
 /// M2 global tip-tilt
+#[derive(UID)]
+#[uid(port = 55_102)]
 pub enum M2GlobalTipTilt {}
-impl UniqueIdentifier for M2GlobalTipTilt {
-    type DataType = [f64; 2];
-    const PORT: u16 = 55_102;
-}
 
 /// Source segment wavefront piston and standard deviation `([m],[m])x7`
 pub enum SegmentWfe<const E: i32 = 0> {}
