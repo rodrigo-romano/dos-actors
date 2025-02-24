8c689ee5acb84b827e14dc2de86a4b7d4cca9b22
Rod Conan <rconan@gmto.org>
2025-02-23 21:28:54 -0800

    GMT_DOS-CLIENTS_M2-CTRL FIX DOC TEST

--- clients/m2-ctrl/src/lib.rs
+++ clients/m2-ctrl/src/lib.rs
@@ -7,12 +7,11 @@ Models of the control systems of the ASMS positioners and voice coil actuators
 
 ```no_run
 use gmt_dos_actors::system::Sys;
-use gmt_dos_clients_m2_ctrl::{ASMS, AsmsPositioners};
+use gmt_dos_clients_m2_ctrl::AsmsPositioners;
 use gmt_fem::FEM;
 
 let mut fem = FEM::from_env()?;
 let positioners = AsmsPositioners::new(&mut fem)?;
-let asms: Sys<ASMS> = ASMS::new(&mut fem)?.build()?;
 # Ok::<(), Box<dyn std::error::Error>>(())
 ```
 
