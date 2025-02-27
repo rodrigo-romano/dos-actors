161c3a475a0955951870f6708414ac76217a41eb
Rod Conan <rconan@gmto.org>
2025-02-26 16:16:16 -0800

    GMT_DOS-CLIENTS_M2-CTRL ASM/FSM SPLIT

---
+++ clients/m2-ctrl/src/asm.rs
@@ -0,0 +1,30 @@
+/*!
+# ASMS Control Systems
+
+Models of the control systems of the ASMS positioners and voice coil actuators
+
+## Example
+
+```no_run
+use gmt_dos_actors::system::Sys;
+use gmt_dos_clients_m2_ctrl::AsmsPositioners;
+use gmt_fem::FEM;
+
+let mut fem = FEM::from_env()?;
+let positioners = AsmsPositioners::new(&mut fem)?;
+# Ok::<(), Box<dyn std::error::Error>>(())
+```
+
+ */
+
+#[cfg(feature = "serde")]
+pub mod nodes;
+
+mod actors_interface;
+mod positioner;
+pub use positioner::Positioners;
+
+pub use actors_interface::AsmSegmentInnerController;
+
+pub mod preprocessor;
+pub use preprocessor::Preprocessor;
--- clients/m2-ctrl/src/lib.rs
+++ clients/m2-ctrl/src/lib.rs
@@ -1,30 +1,8 @@
-/*!
-# ASMS Control Systems
-
-Models of the control systems of the ASMS positioners and voice coil actuators
-
-## Example
-
-```no_run
-use gmt_dos_actors::system::Sys;
-use gmt_dos_clients_m2_ctrl::AsmsPositioners;
-use gmt_fem::FEM;
-
-let mut fem = FEM::from_env()?;
-let positioners = AsmsPositioners::new(&mut fem)?;
-# Ok::<(), Box<dyn std::error::Error>>(())
-```
-
- */
-
-#[cfg(feature = "serde")]
-pub mod nodes;
-
-mod actors_interface;
-mod positioner;
-pub use positioner::AsmsPositioners;
-
-pub use actors_interface::AsmSegmentInnerController;
-
-pub mod preprocessor;
-pub use preprocessor::Preprocessor;
+#[cfg(topend = "ASM")]
+mod asm;
+#[cfg(topend = "ASM")]
+pub use asm::*;
+#[cfg(topend = "FSM")]
+mod fsm;
+#[cfg(topend = "FSM")]
+pub use fsm::*;
