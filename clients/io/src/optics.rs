b81eadf8f052f144b4c22a485b57cdc5642b5671
Rod Conan <rconan@gmto.org>
2025-03-14 21:28:44 +0000

    GMT_DOS-CLIENTS_IO DEV CONDITIONAL COMPILATION FIX

--- clients/io/src/optics.rs
+++ clients/io/src/optics.rs
@@ -155,14 +155,14 @@ impl UniqueIdentifier for Frame<Host> {
 pub mod dispersed_fringe_sensor {
     use interface::{UniqueIdentifier, UID};
 
-    use super::{Cuda, Dev, Frame, Host};
+    use super::{Cuda, Frame, Host};
 
     /// Dispersed Fringe Sensor FFT frame
     ///
     /// The frame is allocated either on the host [Host] or on the device [Dev].
     pub struct DfsFftFrame<T: Cuda>(Frame<T>);
     #[cfg(feature = "crseo")]
-    impl UniqueIdentifier for DfsFftFrame<Dev> {
+    impl UniqueIdentifier for DfsFftFrame<super::Dev> {
         const PORT: u16 = 55_021;
         type DataType = crseo::imaging::Frame;
     }
