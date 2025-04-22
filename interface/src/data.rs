173bd58780feaa8a2bc3530c74c9c21f907d4388
Rod Conan <rconan@gmto.org>
2025-04-22 14:57:05 +0000

    INTERFACE IMPL ALGEBRAIC OPS FOR DATA

--- interface/src/data.rs
+++ interface/src/data.rs
@@ -1,7 +1,7 @@
 use std::{
     fmt::{self, Debug},
     marker::PhantomData,
-    ops::{Deref, Mul},
+    ops::{Add, Deref, Mul, Neg, Sub},
     sync::Arc,
 };
 
@@ -185,6 +185,155 @@ where
 
     fn mul(self, rhs: T) -> Self::Output {
         Data::new(self.0.iter().map(|x| *x * rhs).collect())
+    }
+}
+
+impl<T, U> Add<Data<U>> for Data<U>
+where
+    T: Copy + Add<T, Output = T>,
+    U: UniqueIdentifier<DataType = Vec<T>>,
+{
+    type Output = Data<U>;
+
+    fn add(self, rhs: Data<U>) -> Self::Output {
+        Data::new(
+            self.0
+                .iter()
+                .zip(rhs.0.iter())
+                .map(|(x, y)| *x + *y)
+                .collect(),
+        )
+    }
+}
+impl<T, U> Add<Data<U>> for Vec<T>
+where
+    T: Copy + Add<T, Output = T>,
+    U: UniqueIdentifier<DataType = Vec<T>>,
+{
+    type Output = Data<U>;
+
+    fn add(self, rhs: Data<U>) -> Self::Output {
+        Data::new(
+            self.iter()
+                .zip(rhs.0.iter())
+                .map(|(x, y)| *x + *y)
+                .collect(),
+        )
+    }
+}
+impl<'a, T, U> Add<Data<U>> for &'a [T]
+where
+    T: Copy + Add<T, Output = T>,
+    U: UniqueIdentifier<DataType = Vec<T>>,
+{
+    type Output = Data<U>;
+
+    fn add(self, rhs: Data<U>) -> Self::Output {
+        Data::new(
+            self.iter()
+                .zip(rhs.0.iter())
+                .map(|(x, y)| *x + *y)
+                .collect(),
+        )
+    }
+}
+
+impl<'a, T, U, I> Add<I> for Data<U>
+where
+    T: Copy + Add<T, Output = T> + 'a,
+    U: UniqueIdentifier<DataType = Vec<T>>,
+    I: IntoIterator<Item = &'a T>,
+{
+    type Output = Data<U>;
+
+    fn add(self, rhs: I) -> Self::Output {
+        Data::new(
+            self.0
+                .iter()
+                .zip(rhs.into_iter())
+                .map(|(x, y)| *x + *y)
+                .collect(),
+        )
+    }
+}
+
+impl<T, U> Sub<Data<U>> for Data<U>
+where
+    T: Copy + Sub<T, Output = T>,
+    U: UniqueIdentifier<DataType = Vec<T>>,
+{
+    type Output = Data<U>;
+
+    fn sub(self, rhs: Data<U>) -> Self::Output {
+        Data::new(
+            self.0
+                .iter()
+                .zip(rhs.0.iter())
+                .map(|(x, y)| *x - *y)
+                .collect(),
+        )
+    }
+}
+impl<T, U> Sub<Data<U>> for Vec<T>
+where
+    T: Copy + Sub<T, Output = T>,
+    U: UniqueIdentifier<DataType = Vec<T>>,
+{
+    type Output = Data<U>;
+
+    fn sub(self, rhs: Data<U>) -> Self::Output {
+        Data::new(
+            self.iter()
+                .zip(rhs.0.iter())
+                .map(|(x, y)| *x - *y)
+                .collect(),
+        )
+    }
+}
+impl<'a, T, U> Sub<Data<U>> for &'a [T]
+where
+    T: Copy + Sub<T, Output = T>,
+    U: UniqueIdentifier<DataType = Vec<T>>,
+{
+    type Output = Data<U>;
+
+    fn sub(self, rhs: Data<U>) -> Self::Output {
+        Data::new(
+            self.iter()
+                .zip(rhs.0.iter())
+                .map(|(x, y)| *x - *y)
+                .collect(),
+        )
+    }
+}
+impl<'a, T, U, I> Sub<I> for Data<U>
+where
+    T: Copy + Sub<T, Output = T> + 'a,
+    U: UniqueIdentifier<DataType = Vec<T>>,
+    I: IntoIterator<Item = &'a T>,
+{
+    type Output = Data<U>;
+
+    fn sub(self, rhs: I) -> Self::Output {
+        Data::new(
+            self.0
+                .iter()
+                .zip(rhs.into_iter())
+                .map(|(x, y)| *x - *y)
+                .collect(),
+        )
+    }
+}
+
+impl<T, U> Neg for Data<U>
+where
+    T: Copy + Neg<Output = T>,
+    U: UniqueIdentifier<DataType = Vec<T>>,
+{
+    type Output = Data<U>;
+
+    fn neg(self) -> Self::Output {
+        Data::new(self.0.iter().map(|x| -*x).collect())
     }
 }
 
