4f242fb2537cc322c018988a05b468c742807e0b
Rod Conan <rconan@gmto.org>
2025-04-23 19:57:39 +0000

    INTERFACE CHAIN MACRO  the `chain` macro call client interface for clients that are connected like a chain

--- interface/src/lib.rs
+++ interface/src/lib.rs
@@ -148,3 +148,81 @@ pub fn trim_type_name<T>() -> String {
     }
     trim(type_name::<T>())
 }
+
+/**
+Clients chain
+
+Apply the traits [Read], [Update] and [Write] (and in that order)
+ to clients which inputs and outputs from an uninterrupted chain
+
+A single client:
+```ignore
+chain!(
+    input_uid: input_data;
+    client;
+    output_uid: output_data);
+```
+
+Two clients where the UID of the output of `client1` is
+also the UID of the input of `client2`:
+```ignore
+chain!(
+    client1_input_uid: client_1_input_data;
+    client1;
+    common_uid;
+    client2:
+    client2_output_uid: client2_output_data);
+    );
+```
+
+*/
+#[macro_export]
+macro_rules! chain {
+    ($r:ty:$vr:expr;$e:expr) => {
+        <_ as ::interface::Read<$r>>::read($e, $vr);
+        <_ as ::interface::Update>::update($e);
+    };
+    ($e:expr;$w:ty:$vw:ident) => {
+        let $vw = {
+            <_ as ::interface::Update>::update($e);
+            <_ as ::interface::Write<$w>>::write($e).expect(&format!(
+                "cannot write to {}",
+                ::std::any::type_name::<$w>()
+            ))
+        };
+    };
+    ($r:ty:$vr:expr;$e:expr;$w:ty:$vw:ident) => {
+        let $vw = {
+            <_ as ::interface::Read<$r>>::read($e, $vr);
+            <_ as ::interface::Update>::update($e);
+            <_ as ::interface::Write<$w>>::write($e).expect(&format!(
+                "cannot write to {}",
+                ::std::any::type_name::<$w>()
+            ))
+        };
+    };
+    ($r1:ty:$vr1:expr;$e1:expr;$w1:ty;$e2:expr) => {
+        ::interface::chain!($r1:$vr1;$e1;$w1:data);
+        ::interface::chain!($w1:data;$e2);
+    };
+    ($e1:expr;$w1:ty;$e2:expr) => {
+        ::interface::chain!($e1;$w1:data);
+        ::interface::chain!($w1:data;$e2);
+    };
+    ($e1:expr;$w1:ty;$e2:expr;$w2:ty:$vw2:ident) => {
+        ::interface::chain!($e1;$w1:data);
+        ::interface::chain!($w1:data;$e2;$w2:$vw2);
+    };
+    ($r1:ty:$vr1:expr;$e1:expr;$w1:ty;$e2:expr;$w2:ty:$vw2:ident) => {
+        ::interface::chain!($r1:$vr1;$e1;$w1:data);
+        ::interface::chain!($w1:data;$e2;$w2:$vw2);
+    };
+    ($r1:ty:$vr1:expr;$e1:expr;$w1:ty;$e2:expr;$w2:ty;$e3:expr;$w3:ty:$vw3:ident) => {
+        ::interface::chain!($r1:$vr1;$e1;$w1;$e2;$w2:data);
+        ::interface::chain!($w2:data;$e3;$w3:$vw3);
+    };
+    ($r1:ty:$vr1:expr;$e1:expr;$w1:ty;$e2:expr;$w2:ty;$e3:expr;$w3:ty;$e4:expr;$w4:ty:$vw4:ident) => {
+        ::interface::chain!($r1:$vr1;$e1;$w1;$e2;$w2;$e3;$w3:data);
+        ::interface::chain!($w3:data;$e4;$w4:$vw4);
+    };
+}
